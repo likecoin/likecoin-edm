5d3db136e19fe0943386cd2228fa5e30bd994d65
Ng Wing Tat, David <i@ngwingt.at>
2020-12-30 16:38:19 +0800

    🏷️ Fix missing definition file for 'react'

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { MjmlColumn, MjmlText } from 'mjml-react';
 
 import { BasicSection } from './section';
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { MjmlColumn, MjmlImage } from 'mjml-react';
 
 import { BasicSection } from './section';

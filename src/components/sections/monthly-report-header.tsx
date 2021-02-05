ccf4f4e83892cb0c202d6e1893035d994bdad0a4
Ng Wing Tat, David <i@ngwingt.at>
2021-02-05 13:55:30 +0800

    ✨ Add greeting to monthly report header

--- src/components/sections/monthly-report-header.tsx
+++ src/components/sections/monthly-report-header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { MjmlColumn, MjmlText } from 'mjml-react';
+import { MjmlColumn, MjmlText, MjmlWrapper } from 'mjml-react';
 import { FormattedDateParts, FormattedMessage, useIntl } from 'react-intl';
 
 import * as Colors from '../../constants/colors';
@@ -21,43 +21,57 @@ export const MonthlyReportHeaderSection = (
     title = '',
     timestamp = 0,
     likerID = '',
-    displayName = '',
+    displayName = likerID,
     avatarSrc = '',
     isCivicLiker = false,
   } = props;
   const intl = useIntl();
   return (
-    <BasicSection backgroundColor={Colors.LikeGreen}>
-      <MjmlColumn>
-        <Avatar
-          src={avatarSrc}
-          likerID={likerID}
-          displayName={displayName}
-          isCivicLiker={isCivicLiker}
-          size={108}
-          align="left"
-        />
-        <MjmlText fontSize={28} fontWeight={600} color="white" paddingTop={16}>
-          {title}
-        </MjmlText>
-        <MjmlText fontSize={18} fontWeight={600} color={Colors.LikeCyan}>
-          <FormattedDateParts
-            value={new Date(Number(timestamp))}
-            year="numeric"
-            month={intl.locale === 'en' ? 'short' : 'numeric'}
+    <MjmlWrapper>
+      <BasicSection backgroundColor={Colors.LikeGreen}>
+        <MjmlColumn>
+          <Avatar
+            src={avatarSrc}
+            likerID={likerID}
+            displayName={displayName}
+            isCivicLiker={isCivicLiker}
+            size={108}
+            align="left"
+          />
+          <MjmlText
+            fontSize={28}
+            fontWeight={600}
+            color="white"
+            paddingTop={16}
           >
-            {parts => (
-              <FormattedMessage
-                id="report.monthly.header.date"
-                values={{
-                  month: parts[0].value,
-                  year: parts[2].value,
-                }}
-              />
-            )}
-          </FormattedDateParts>
-        </MjmlText>
-      </MjmlColumn>
-    </BasicSection>
+            {title}
+          </MjmlText>
+          <MjmlText fontSize={18} fontWeight={600} color={Colors.LikeCyan}>
+            <FormattedDateParts
+              value={new Date(Number(timestamp))}
+              year="numeric"
+              month={intl.locale === 'en' ? 'short' : 'numeric'}
+            >
+              {parts => (
+                <FormattedMessage
+                  id="report.monthly.header.date"
+                  values={{
+                    month: parts[0].value,
+                    year: parts[2].value,
+                  }}
+                />
+              )}
+            </FormattedDateParts>
+          </MjmlText>
+        </MjmlColumn>
+      </BasicSection>
+      <BasicSection backgroundColor="white" paddingBottom={0}>
+        <MjmlColumn>
+          <MjmlText color={Colors.LikeGreen} fontSize={16} fontWeight={600}>
+            <FormattedMessage id="greeting" values={{ name: displayName }} />
+          </MjmlText>
+        </MjmlColumn>
+      </BasicSection>
+    </MjmlWrapper>
   );
 };

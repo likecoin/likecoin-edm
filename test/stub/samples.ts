613574bba223bd5159a4784fa86023be9a850c09
shellyyg <chih-ching.yang@whu.edu>
2021-03-22 10:57:09 +0100

    ✅ Update transaction test to incl. remark params

--- test/stub/samples.ts
+++ test/stub/samples.ts
@@ -5,3 +5,4 @@ export const SAMPLE_RICH_TEXT = '<p>Lorem <b>Ipsum</b></p>';
 export const SAMPLE_TEXT = 'Lorem Ipsum';
 export const SAMPLE_TIMESTAMP = 1609459860000;
 export const SAMPLE_URL = 'https://example.com';
+export const SAMPLE_REMARK = 'likecoin remark';
--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -1,6 +1,10 @@
 import { getTransactionTemplate } from '../src';
 
-import { SAMPLE_URL, SAMPLE_LIKER_DISPLAY_NAME } from './stub/samples';
+import {
+  SAMPLE_URL,
+  SAMPLE_LIKER_DISPLAY_NAME,
+  SAMPLE_REMARK,
+} from './stub/samples';
 
 describe('Render new transaction templates', () => {
   it('Default', () => {
@@ -11,6 +15,7 @@ describe('Render new transaction templates', () => {
         fromDisplayName: SAMPLE_LIKER_DISPLAY_NAME,
         viewTxURL: SAMPLE_URL,
         amount: '100',
+        remarks: SAMPLE_REMARK,
       },
       { minify: false }
     );

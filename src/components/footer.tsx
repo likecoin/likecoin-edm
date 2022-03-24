712723e764fbcf6242d88804eb016a6ba0759407
Ng Wing Tat, David <i@ngwingt.at>
2022-03-24 22:02:14 +0800

    ✨ Update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,66 +1,90 @@
 import * as React from 'react';
 import {
   MjmlColumn,
+  MjmlDivider,
+  MjmlImage,
   MjmlSocial,
   MjmlSocialElement,
   MjmlText,
 } from 'mjml-react';
 
+import * as Colors from '../constants/colors';
 import { getAssetPath } from '../utils/url';
 
 import { BasicSection } from './sections/basic';
 import { HelpCenterLink } from './help-center-link';
 import { UnsubscribeLink } from './unsubscribe-link';
+import { FormattedMessage, useIntl } from 'react-intl';
+
+const socialElements = [
+  {
+    key: 'discord',
+    href: 'https://discord.gg/W4DQ6peZZZ',
+    assetPath: '/social-elements/v2/discord.png',
+  },
+  {
+    key: 'github',
+    href: 'https://github.com/likecoin',
+    assetPath: '/social-elements/v2/github.png',
+  },
+  {
+    key: 'medium',
+    href: 'https://medium.com/likecoin',
+    assetPath: '/social-elements/v2/medium.png',
+  },
+];
 
 export const FooterSection = () => {
+  const intl = useIntl();
   return (
     <BasicSection
       backgroundColor="white"
       backgroundUrl={undefined}
-      paddingTop={24}
+      paddingTop={32}
       paddingBottom={24}
     >
       <MjmlColumn>
+        <MjmlDivider
+          paddingTop={32}
+          paddingBottom={32}
+          borderColor={Colors.PaleCyan}
+          borderWidth={2}
+        />
+        <MjmlText
+          paddingTop={24}
+          paddingBottom={32}
+          color={Colors.Grey4A}
+          fontSize={16}
+          align="center"
+        >
+          <FormattedMessage id="footer.follow-us" />
+        </MjmlText>
+        <MjmlImage
+          width={220}
+          src={getAssetPath(
+            `/social-elements/v2/twitter-large-${intl.locale}.png`
+          )}
+          href="https://twitter.com/likecoin"
+        />
         <MjmlSocial
           mode="horizontal"
-          paddingTop={8}
-          paddingBottom={8}
+          paddingTop={16}
+          paddingBottom={16}
           innerPadding="6px"
-          iconSize="28px"
-          iconHeight="28px"
-          borderRadius={24}
+          iconSize="32px"
+          iconHeight="32px"
+          borderRadius={16}
           containerBackgroundColor="white"
         >
-          <MjmlSocialElement
-            href="https://discord.gg/W4DQ6peZZZ"
-            src={getAssetPath('/social-elements/discord.png')}
-          >
-            {null}
-          </MjmlSocialElement>
-          <MjmlSocialElement
-            href="https://medium.com/likecoin"
-            src={getAssetPath('/social-elements/medium.png')}
-          >
-            {null}
-          </MjmlSocialElement>
-          <MjmlSocialElement
-            src={getAssetPath('/social-elements/twitter.png')}
-            href="https://twitter.com/likecoin"
-          >
-            {null}
-          </MjmlSocialElement>
-          <MjmlSocialElement
-            src={getAssetPath('/social-elements/github.png')}
-            href="https://github.com/likecoin"
-          >
-            {null}
-          </MjmlSocialElement>
-          <MjmlSocialElement
-            src={getAssetPath('/social-elements/facebook.png')}
-            href="https://www.facebook.com/groups/likecoin"
-          >
-            {null}
-          </MjmlSocialElement>
+          {socialElements.map(({ key, href, assetPath }) => (
+            <MjmlSocialElement
+              key={key}
+              href={href}
+              src={getAssetPath(assetPath)}
+            >
+              {null}
+            </MjmlSocialElement>
+          ))}
         </MjmlSocial>
         <MjmlText align="center" padding={4}>
           <HelpCenterLink />
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,6 +1,7 @@
 export const LikeGreen = '#28646e';
 export const LikeCyan = '#50e3c2';
 export const LighterCyan = '#aaf1e7';
+export const PaleCyan = '#D7ECEC';
 
 export const Grey4A = '#4a4a4a';
 export const Grey9B = '#9b9b9b';

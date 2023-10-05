137ea8971974210820e89b4c6221addfefd9de44
Ng Wing Tat, David <i@ngwingt.at>
2023-10-05 18:03:13 +0800

    🐛 Make static asset URLs varies with port

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const ASSETS_ROOT =
   process.env.NODE_ENV === 'production'
     ? 'https://static.like.co/edm'
-    : 'http://localhost:3000/static';
+    : `http://localhost:${process.env.PORT || '3000'}/static`;
 
 export const DEFAULT_AVATAR_URL = `${ASSETS_ROOT}/default-avatar.jpg`;
 
